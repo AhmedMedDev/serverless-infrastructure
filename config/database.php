42943f94b974f18261274bfd9079147e39d7c8bd
AhmedMedDev <ahmedmed.dev@gmail.com>
2024-02-20 10:22:42 +0200

    MedToolKits : Created Aurora Connection

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ return [
     |--------------------------------------------------------------------------
     | Database Connections
     |--------------------------------------------------------------------------
-    |
+    | admin / eMYajqNp1LvG4gEjWwwr / aurora-cluster-instance-1.cleu4grfvxuc.me-south-1.rds.amazonaws.com / aurora_db
     | Here are each of the database connections setup for your application.
     | Of course, examples of configuring each database platform that is
     | supported by Laravel is shown below to make development simple.
